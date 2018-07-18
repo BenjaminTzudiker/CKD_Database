2d3029123cda104b0d10a12d35ae702db08cc6d3
Benjamin Tzudiker <benjamintzudiker@MacBook-Air-5.local>
2018-07-18 12:59:07 -0700

    Changed index script to use unique indexes.

--- Postgres/Create_Index.sql
+++ Postgres/Create_Index.sql
@@ -1,12 +1,13 @@
 \c ckd;
 
-create index index_site_source on site_source (site_source);
-create index index_patient on patient (patient_id);
-create index index_encounter on encounter (encounter_id, patient_id);
-create index index_diagnosis on diagnosis (encounter_id);
-create index index_vital_sign on vital_sign (encounter_id);
-create index index_lab on lab (encounter_id);
-create index index_medication on medication (encounter_id);
-create index index_patient_condition on patient_condition (patient_id);
-create index index_procedure on procedure (encounter_id);
-create index index_social_history on social_history (encounter_id);
+create unique index index_site_source on site_source (site_source);
+create unique index index_patient on patient (patient_id);
+create unique index index_encounter on encounter (encounter_id);
+create index index_encounter_secondary on encounter(patient_id);
+create index index_diagnosis_secondary on diagnosis (encounter_id);
+create index index_vital_sign_secondary on vital_sign (encounter_id);
+create index index_lab_secondary on lab (encounter_id);
+create index index_medication_secondary on medication (encounter_id);
+create index index_patient_condition_secondary on patient_condition (patient_id);
+create index index_procedure_secondary on procedure (encounter_id);
+create index index_social_history_secondary on social_history (encounter_id);
